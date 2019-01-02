26505e3b416f982ac20b00320cc6f08c71caf106
icepoint1999 <351711778@qq.com>
2019-01-02 18:11:40 +0800

    Updates

---
+++ precache-manifest.99052cd6e24ab6230cbb3effaa39383d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a940fe89dbfe9d1d89fc1aa0488fe032",
+    "url": "/at-ui-react/static/media/feather.a940fe89.ttf"
+  },
+  {
+    "revision": "66cbb621b431bf32041a5c478e5539c0",
+    "url": "/at-ui-react/static/media/feather.66cbb621.woff"
+  },
+  {
+    "revision": "5fad700adc948cb51404d55833347f51",
+    "url": "/at-ui-react/static/media/feather.5fad700a.eot"
+  },
+  {
+    "revision": "023ba0824a99109c4a63622228dd6354",
+    "url": "/at-ui-react/static/media/feather.023ba082.svg"
+  },
+  {
+    "revision": "e2136b2b9e87727efade",
+    "url": "/at-ui-react/static/js/runtime~main.e2136b2b.js"
+  },
+  {
+    "revision": "fe257922c23cfbecba88",
+    "url": "/at-ui-react/static/js/main.fe257922.chunk.js"
+  },
+  {
+    "revision": "dae9139e4906c3092ab9",
+    "url": "/at-ui-react/static/js/1.dae9139e.chunk.js"
+  },
+  {
+    "revision": "fe257922c23cfbecba88",
+    "url": "/at-ui-react/static/css/main.c1917f95.chunk.css"
+  },
+  {
+    "revision": "a5050cc16ea26720b49e0fb4f266fd69",
+    "url": "/at-ui-react/index.html"
+  }
+];
